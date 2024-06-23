fa79caa530437fb89a29434b448218cdac842cac
viniciusmagao <vincius.engredes@gmail.com>
2024-06-23 14:19:16 -0300

    Primeiro commit do script.js as 14h18

--- src/script.js
+++ src/script.js
@@ -10,6 +10,33 @@ function changeText(id, text) {
 // Daqui para baixo voce ira escrever
 // o código para resolver o desafio
 
+let idOffSet = 0; //Id de inicio da lista de pokemons
+let idLimit = 500; //Id da quantidade de pokemons para retornar
+let idPokemon = 0;
+
+//Funcao pra buscar os dados na API a partir da url passada
+async function fetchPokemon () {
+  const url = `https://pokeapi.co/api/v2/pokemon///?offset=${idOffSet}&limit=${idLimit}`;
+  const respostaAPI = await fetch(url).then((response) => response.json());
+  //console.log(respostaAPI);
+  return respostaAPI;
+}
+
+//Busca os dados do pokemon por meio da url que vem no objeto da chamada inicial
+async function atualizaPokemon(idPokemon) {
+
+  const pokemon = await fetchPokemon();
+  
+  id = idPokemon +1;
+  url = pokemon.results[idPokemon].url;
+  text = pokemon.results[idPokemon].name;
+  
+  changeImage(id, url);
+  changeText(id, text);
+}
+
+
+
 function previousPokemon() {
   alert("Pokemon Anterior");
   //abra o terminal em inspecionar no chrome para visualizar
